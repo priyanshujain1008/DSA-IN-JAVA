8062d963aa10db58c9fd99c78f702874d808fc96
priyanshujain1008 <cu23250461@coeruniversity.ac.in>
2026-06-25 18:22:28 +0530

    Time: 0 ms (100%), Space: 79.9 MB (93.97%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int n=arr.length;
+        int res=-1;
+        int low=0, high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]<arr[mid+1]){
+                low=mid+1;
+            }else{
+                res=mid;
+                high=mid-1;
+            }
+
+        }return res;
+    }
+}

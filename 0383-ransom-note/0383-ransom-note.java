8672dca2da15c18fdb32e1e04eba4a237d133c1e
priyanshujain1008 <cu23250461@coeruniversity.ac.in>
2026-07-10 18:41:54 +0530

    Time: 16 ms (18.62%), Space: 47 MB (27.89%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,28 +1,29 @@
 class Solution {
 
-    boolean fun(Map<Character,Integer> have, Map<Character,Integer> need ){
-         for (Map.Entry<Character, Integer> i : need.entrySet()) {
-            char c=i.getKey();
-            int fneed =i.getValue();
-            int fhave =have.getOrDefault(c,0);
+    public boolean fun(Map<Character,Integer> have, Map<Character,Integer> need){
+        for(char c: need.keySet()){
+            int fneed=need.get(c);
+            int fhave=have.getOrDefault(c,0);
+
             if(fhave<fneed){
                 return false;
             }
+                
         }return true;
     }
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character,Integer>have=new HashMap<>(); //kitna hai
-        Map<Character,Integer>need=new HashMap<>(); //chaiye kitna
+        Map<Character,Integer> have= new HashMap<>();
+        Map<Character,Integer> need= new HashMap<>();
 
         for(int i=0;i<ransomNote.length();i++){
-            char ch = ransomNote.charAt(i);
-            need.put(ch, need.getOrDefault(ch, 0) + 1);
-           
+            char ch=ransomNote.charAt(i);
+            need.put(ch, need.getOrDefault(ch , 0)+1);
         }
-        for(int i=0;i<magazine.length();i++){
-            char ch = magazine.charAt(i);
-            have.put(ch, have.getOrDefault(ch, 0) + 1);
-    }return fun(have,need);
 
+        for(int i=0;i<magazine.length();i++){
+            char ch=magazine.charAt(i);
+            have.put(ch, have.getOrDefault(ch , 0)+1);
         }
+        return fun(have,need);
+    }
 }

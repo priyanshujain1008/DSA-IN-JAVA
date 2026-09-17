56ed23cd787ba349e75533d92ea985770a5d3d74
priyanshujain1008 <cu23250461@coeruniversity.ac.in>
2026-09-17 17:25:51 +0530

    Time: 1 ms (99.84%), Space: 46.2 MB (98.69%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -1,5 +1,9 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
+
+        // iske test cases toh samajh gya lekin isme humn last mein list ko string mein convert like a String. valueOf(i)   ----->>>> meanns converting no. into string values
+        
+         //aur loop humne 1 se chalayi bcz question mein de rkha tha 1-indexed se 
        List<String> answer=new ArrayList<>();
        for(int i=1;i<=n;i++){
         if(i%3==0 && i%5==0){
